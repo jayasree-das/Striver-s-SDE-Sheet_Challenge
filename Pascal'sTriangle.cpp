29db6d6ae978e7b49fab38f25cc468b2bcaf29af
Jayasree Das <94794846+jayasree-das@users.noreply.github.com>
2023-06-06 09:17:31 +0530

    Update Pascal'sTriangle.cpp

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -1,3 +1,32 @@
+//Brute Force Approach
+
+T.C:- O(n*n*n)
+S.C:- O(1)
+#include <bits/stdc++.h>
+long long int nCr(int n, int r) {
+    long long int res = 1;
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+vector<vector<long long int>> printPascal(int n) 
+{
+  vector<vector<long long int>>ans;
+  //Store the entire Pascal's Triangle
+  for (int row = 1; row <= n; row++) {
+    vector<long long int> temp; // temporary list
+    for (int col = 1; col <= row; col++) {
+      temp.push_back(nCr(row - 1, col - 1));
+    }
+    ans.push_back(temp);
+  }
+  return ans;
+}
+
+
+
 //Optimal solution
 T.C:- O(n2)
 S.C:- O(1)
