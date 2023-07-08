8d48e8b169cdff233be0c5b601bef74d0d0be81a
Jayasree Das <94794846+jayasree-das@users.noreply.github.com>
2023-07-08 10:49:10 +0530

    Update KthPermutationSequence.cpp

--- KthPermutationSequence.cpp
+++ KthPermutationSequence.cpp
@@ -33,3 +33,28 @@ string kthPermutation(int n, int k) {
 
 
 //Optimal solution
+T.C:-O(n^2)
+S.C:-O(n)
+
+string kthPermutation(int n, int k) {
+    vector<int>numbers;
+    int fact=1;
+    for(int i=1;i<n;i++){
+        fact=fact*i;
+        numbers.push_back(i);
+    }
+    numbers.push_back(n);
+    string ans="";
+    //0 based indexing
+    k=k-1;
+
+    while(true){
+        ans=ans+to_string(numbers[k/fact]);
+        numbers.erase(numbers.begin()+k/fact);
+        if(numbers.size()==0) break;
+        k=k%fact;
+        fact=fact/numbers.size();
+    }
+
+    return ans;
+}
