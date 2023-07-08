63a5a78299b0e85d61a185f25a349b2af358bf74
Jayasree Das <94794846+jayasree-das@users.noreply.github.com>
2023-07-08 09:15:32 +0530

    Create KthPermutationSequence.cpp

---
+++ KthPermutationSequence.cpp
@@ -0,0 +1,35 @@
+//Brute Force (Generating all permutations recursively and then returning)
+T.C:-O(n*n!)+O(n! log n!)
+S.C:-O(n) since we are storing the permutations in a vector
+
+#include<bits/stdc++.h>
+void solve(string input,int n,int index, vector<string>& ans){
+    if(index>=n){
+        ans.push_back(input);
+        return;
+    }
+    for(int i=index;i<n;i++){
+        swap(input[i],input[index]);
+        solve(input,n,index+1,ans);
+        swap(input[i],input[index]);
+    }
+    return;
+}
+string kthPermutation(int n, int k) {
+    // input string create
+    string input = "";
+    for(int i=1;i<=n;i++){
+        char ch = i+48;
+        input+=ch;
+    }
+    //storing all permutations
+    vector<string> ans;
+    solve(input,n,0,ans);
+    //sorting all the stored permutations
+    sort(ans.begin(),ans.end());
+    //consider 0 based indexing, so kth sequence will be at (k-1) index
+    return ans[k-1];
+}
+
+
+//Optimal solution
