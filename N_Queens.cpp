522f44cc00f0c2d35395f6670867b45a5464d9d9
Jayasree Das <94794846+jayasree-das@users.noreply.github.com>
2023-07-12 09:20:53 +0530

    Update N_Queens.cpp

--- N_Queens.cpp
+++ N_Queens.cpp
@@ -55,3 +55,48 @@ vector<vector<int>> solveNQueens(int n) {
     solve(0,n,board,ans);
     return ans;
 }
+
+
+
+
+
+//Optimised backtracking
+T.C:-O(n)
+S.C:-O(n)
+
+#include <bits/stdc++.h>
+void solve(int col,int n,vector<vector<int>>& ans,vector<int>& leftrow,vector<int>& upperDiag,
+vector<int>& lowerDiag,vector<vector<int>>& board){
+    if(col==n){
+        vector<int>temp;
+        for(auto it: board){
+            for(int i=0;i<it.size();i++)
+            temp.push_back(it[i]);
+        }
+        ans.push_back(temp);
+        return;
+    }
+
+    for(int row=0;row<n;row++){
+        if(leftrow[row]==-1 && lowerDiag[row+col]==-1 && upperDiag[n-1+col-row]==-1){
+            board[row][col]=1;
+            leftrow[row]=1;
+            lowerDiag[row+col]=1;
+            upperDiag[n-1+col-row]=1;
+            solve(col+1,n,ans,leftrow,upperDiag,lowerDiag,board);
+            //while returning back
+            board[row][col]=0;
+            leftrow[row]=-1;
+            lowerDiag[row+col]=-1;
+            upperDiag[n-1+col-row]=-1;
+        }
+    }
+}
+
+vector<vector<int>> solveNQueens(int n) {
+    vector<vector<int>>ans;//to store ans
+    vector<vector<int>>board(n,vector<int>(n,0));
+    vector<int>leftrow(n,-1),upperDiag(2*n-1,-1),lowerDiag(2*n-1,-1);
+    solve(0,n,ans,leftrow,upperDiag,lowerDiag,board);
+    return ans;
+}
