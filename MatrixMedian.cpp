635f68aa51e40a6c86e9f463c80dc1aea8b43d6e
Jayasree Das <94794846+jayasree-das@users.noreply.github.com>
2023-07-13 20:27:23 +0530

    Update MatrixMedian.cpp

--- MatrixMedian.cpp
+++ MatrixMedian.cpp
@@ -19,3 +19,42 @@ int getMedian(vector<vector<int>> &matrix)
 
 
 //Optimal solution (Using Binary Search)
+T.C:-O(n * log(m) * log(2^32) which is approximately o(n * log m)
+S.C:-O(1)
+int countsmallerthanMid(int mid,vector<int>& row){
+    int left=0,right=row.size()-1;
+    while(left<=right)
+    {
+        int md=(left+right)/2;
+        if (row[md] <= mid)
+        {
+            left= md+1;
+        }
+        else
+        {
+            right=md-1;
+        }
+    }
+    return left;
+}
+
+int getMedian(vector<vector<int>> &matrix)
+{
+    int n=matrix.size();
+    int m=matrix[0].size();
+    int low=1,high=1e9;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int cnt=0;
+        //counting the no of elements lesser than our mid value
+        for(int i=0;i<n;i++){
+            cnt+=countsmallerthanMid(mid,matrix[i]);
+        }
+        //median should be any index atleast >=(n*m)/2
+        //so if the no of elements smaller than our mid is less than or equal to (n*m)/2
+        //we need to move our search space to the right
+        if(cnt<=(n*m)/2) low=mid+1;
+        else high=mid-1;
+    }
+    return low;
+}
